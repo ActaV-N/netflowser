4d502f4a156bf6b917778a0f7edbacd145ab39c1
actav-n <gksdmltk02@gmail.com>
2023-07-16 00:55:14 +0900

    added vite config for build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: ".",
+  publicDir: "./static",
   build: {
     outDir: "dist",
     rollupOptions: {
       input: {
         content: "./content/index.tsx",
+        popup: "./popup/index.html",
       },
       output: {
         format: "es",
